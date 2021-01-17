f341ae8a410d722fb0275d393dfec6c093b4af1b
Phillbert Tan <philly@phillytan.xyz>
2021-01-16 17:17:59 -0800

    Add Code Entry

--- screens/HomeScreen/CodeScreen.tsx
+++ screens/HomeScreen/CodeScreen.tsx
@@ -1,15 +1,38 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
 import { Button, Input } from 'react-native-elements'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-function HomeScreen({ navigation }) {
+function CodeScreen({ navigation }) {
+
+  const [ code, setCode ] = React.useState()
+  const [id, setId] = React.useState('')
+  const [token, setToken] = React.useState('')
+  React.useEffect(() => {
+    (async () => {
+      let id = await AsyncStorage.getItem('@id')
+      let token = await AsyncStorage.getItem('@token')
+      if (!id || !token) {
+        const res = await fetch('https://us-central1-geotrace-301902.cloudfunctions.net/makeUser')
+        const obj = await res.json()
+        id = obj.id
+        token = obj.token
+        await AsyncStorage.setItem('@id', id)
+        await AsyncStorage.setItem('@token', token)
+      }
+      setId(id)
+      setToken(token)
+    })()
+  }, [])
+
   return (
     <View>
       <View style={{ padding: 16 }}>
         <Text style={{ fontWeight: '300', fontSize: 24 }}>Please enter a Contact Tracing code to check in.</Text>
         <Text />
         <Input
-          placeholder='Contact Tracing Code'
+          placeholder={`Contact Tracing Code`}
+          onChangeText={(value) => setCode(value)}
         />
         <Text />
         <Button
@@ -23,6 +46,7 @@ function HomeScreen({ navigation }) {
             color: "white"
           }}
           onPress={() => {
+            fetch(`https://us-central1-geotrace-301902.cloudfunctions.net/makeExchange?userId=${id}&userToken=${token}&establishmentToken=${code}`)
             navigation.navigate('Home')
           }}
         />
@@ -46,4 +70,4 @@ function HomeScreen({ navigation }) {
   );
 }
 
-export default HomeScreen
+export default CodeScreen
